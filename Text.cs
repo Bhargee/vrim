eabfd4b10472cdaab60313b42257833b7e1ea59b
Bhargava Manja <bhargee.manja@gmail.com>
2015-03-27 22:19:14 -0700

    First Commit

---
+++ Text.cs
@@ -0,0 +1,125 @@
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using Slusser.Collections.Generic;
+
+namespace vrim
+{
+
+	/* 
+	 * Class Text manages the "backend" of vrim, exposing an API for basic text manipulation
+	 * (moving the point, entering chars, deleting chars, etc)
+	 * there is a bijection between open files and Text objects
+	 */
+	public class Text
+	{
+		private GapBuffer<char> buffer;
+		private string filename =  null;
+		private int point = 0;
+
+		private static int linewidth = 80;
+
+		public enum Direction {Up, Left, Right, Down};
+
+		public string Filename {
+			get { return this.filename;}
+			set { this.filename = value;}
+		}
+
+		public int Point {
+			get { return this.point;}
+			set { this.point = value;}
+		}
+
+		public Text ()
+		{
+			buffer = new GapBuffer<char>();
+		}
+
+		public Text (String filename) 
+		{
+			buffer = new GapBuffer<char> ();
+			buffer.InsertRange (point, File.ReadAllText (filename));  
+			this.filename = filename;
+		}
+
+		public override string ToString ()
+		{
+			char [] res_array = new char[buffer.Count];
+			buffer.CopyTo (res_array, 0);
+			string res = new string (res_array);
+			return res;
+		}
+
+		public void Insert(string data)
+		{
+			if (point == buffer.Count) 
+				buffer.AddRange (data);
+			else
+				buffer.InsertRange (point, data);
+
+			point += data.Length;
+		}
+
+		public int MovePoint(Direction d, int amount)
+		{
+			if (d == Direction.Up) {
+				if ((point - (amount * linewidth)) >= 0) {
+					point -= (amount * linewidth);
+				}
+			} else if (d == Direction.Right) {
+				if ((point + amount) < buffer.Count) {
+					point += amount;
+				}
+			} else if (d == Direction.Down) {
+				if ((point + (amount * linewidth)) < buffer.Count) {
+					point += (amount * linewidth);
+				}
+			} else if (d == Direction.Left) {
+				if ((point - amount) >= 0) {
+					point -= amount;
+				}
+			}
+			return point;
+		}
+
+		public char GetChar() 
+		{
+			return buffer [this.point];
+		}
+
+		public string Display(bool windows) 
+		{
+			int added = 0;
+			char newline = '\n';
+			if (windows)
+				newline = '\r';
+			int num_newlines = buffer.Count / linewidth;
+			char[] res = new char[buffer.Count + num_newlines];
+			for (int i = 0; i < buffer.Count-1; i++) {
+				if ((i % linewidth) == 0) {
+					res[i+added] = newline;
+					added++;
+					res[i+added] = buffer[i];
+				}
+				else
+					res[i+added] = buffer[i];
+			}
+			return new string(res);
+		}
+
+		public List<int> RegexSearch(string pattern)
+		{
+			Match m = Regex.Match (buffer.ToString(), pattern);
+			Console.WriteLine (m.Value);
+			List<int> res = new List<int> ();
+			while (m.Success) {
+				res.Add(m.Index);
+				m = m.NextMatch();
+			}
+			return res;
+		}
+	}
+}
+
---
+++ Vrim.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace vrim
+{
+	class Vrim
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Entrypoint to vrim editor");
+			Text t = new Text ("/Users/Bhargava/Projects/Vrim/test.txt");
+			while (true) {
+				string line = Console.ReadLine();
+				if (line.Contains("q"))
+					break;
+				else if (line.Equals("l"))
+					t.MovePoint(Text.Direction.Right, 1);
+				else if (line.Equals("k"))
+					t.MovePoint(Text.Direction.Up, 1);
+				else if (line.Equals("j"))
+					t.MovePoint(Text.Direction.Down, 1);
+				else if (line.Equals("h"))
+					t.MovePoint(Text.Direction.Left, 1);
+				else
+					Console.WriteLine(t.GetChar());
+			}
+			//Console.Write (t.Display(false));
+			Console.WriteLine((t.RegexSearch(@"Robinson")).Count);
+
+		}
+	}
+}
